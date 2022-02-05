27a1601b5fbd347971dba45164647e422e1b2b80
Rachel Miles <rachmiles@gmail.com>
2022-02-05 15:14:55 +0000

    Implemented functionality for isFromManchester in exercise 002

--- src/main/scala/com/techreturners/exercise002/Exercise002.scala
+++ src/main/scala/com/techreturners/exercise002/Exercise002.scala
@@ -1,9 +1,13 @@
 package com.techreturners.exercise002
 
+case class Person(firstName: String, lastName: String, city: String, age: Int)
+
 class Exercise002 {
 
-  def isFromManchester(/*person: Person*/): Boolean = false
+  def isFromManchester(person: Person): Boolean = person.city == "Manchester"
 
-  def canWatchFilm(/*person: Person, ageLimit: Int */): Boolean = false
+  def canWatchFilm(/*person: Person, ageLimit: Int */): Boolean = {
+    false
+  }
 
 }
