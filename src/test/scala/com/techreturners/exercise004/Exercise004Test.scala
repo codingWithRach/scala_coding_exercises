7922127b9c48c80d918995e93ecdb6346b2ec522
Rachel Miles <rachmiles@gmail.com>
2022-02-06 18:01:33 +0000

    Original tests completed in exercise 004

--- src/test/scala/com/techreturners/exercise004/Exercise004Test.scala
+++ src/test/scala/com/techreturners/exercise004/Exercise004Test.scala
@@ -11,12 +11,12 @@ class Exercise004Test extends AnyFlatSpec{
     assert(exercise004.getDateTimeWithGigaSecond == LocalDateTime.of(2053, Month.MARCH, 27, 1, 46, 40))
   }
 
-  it should "be returned when constructed with a date and time" ignore {
+  it should "be returned when constructed with a date and time" in {
     val exercise004 = new Exercise004(LocalDateTime.of(2021, Month.MARCH, 4, 23, 22, 0, 0))
     assert(exercise004.getDateTimeWithGigaSecond == LocalDateTime.of(2052, Month.NOVEMBER, 11, 1, 8, 40))
   }
 
-  it should "be returned when constructed with a date and time with day roll over" ignore {
+  it should "be returned when constructed with a date and time with day roll over" in {
     val exercise004 = new Exercise004(LocalDateTime.of(2021, Month.JANUARY, 24, 23, 59, 59, 0))
     assert(exercise004.getDateTimeWithGigaSecond == LocalDateTime.of(2052, Month.OCTOBER, 3, 1, 46, 39))
   }
