40da38d27f458a4dda4976bb33f0a6dbe8e1a0db
Rachel Miles <rachmiles@gmail.com>
2022-02-06 11:38:12 +0000

    First test completed in exercise 004

--- src/main/scala/com/techreturners/exercise004/Exercise004.scala
+++ src/main/scala/com/techreturners/exercise004/Exercise004.scala
@@ -4,10 +4,11 @@ import java.time.{LocalDate, LocalDateTime}
 
 class Exercise004(var dateTime: LocalDateTime) {
 
-  // Hint take a look at Scala auxiliary constructors
-  // https://docs.scala-lang.org/overviews/scala-book/classes-aux-constructors.html
-  // Try constructing a "LocalDateTime" from a "LocalDate"
-  // You might need to default the time to be something like midnight
+  // auxiliary constructor
+  def this(dateTime: LocalDate) = {
+    this(dateTime.atStartOfDay())
+  }
 
-  def getDateTimeWithGigaSecond: LocalDateTime = null
+  def getDateTimeWithGigaSecond: LocalDateTime = dateTime.plusSeconds(1000000000)
+
 }
