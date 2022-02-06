33d9fdb8da8260b6fcccca50a78d1cbdacf88a10
Rachel Miles <rachmiles@gmail.com>
2022-02-06 19:39:52 +0000

    Additional tests completed in exercise 004, and an attempt at error handling

--- src/main/scala/com/techreturners/exercise004/Exercise004.scala
+++ src/main/scala/com/techreturners/exercise004/Exercise004.scala
@@ -1,12 +1,20 @@
 package com.techreturners.exercise004
 
-import java.time.{LocalDate, LocalDateTime}
+import java.time.{DateTimeException, LocalDate, LocalDateTime}
 
 class Exercise004(var dateTime: LocalDateTime) {
 
   // auxiliary constructor
   def this(dateTime: LocalDate) = this(dateTime.atStartOfDay())
 
-  def getDateTimeWithGigaSecond: LocalDateTime = dateTime.plusSeconds(1000000000)
-
+  def getDateTimeWithGigaSecond: LocalDateTime = {
+    try {
+      dateTime.plusSeconds(1000000000)
+    } catch {
+      // there must be something more appropriate to return for an invalid date/time!
+      case err: DateTimeException =>
+        println("Exception: invalid date/time")
+        LocalDateTime.of(0, 1, 1, 0, 0, 0)
+    }
+  }
 }
--- src/test/scala/com/techreturners/exercise004/Exercise004Test.scala
+++ src/test/scala/com/techreturners/exercise004/Exercise004Test.scala
@@ -20,4 +20,24 @@ class Exercise004Test extends AnyFlatSpec{
     val exercise004 = new Exercise004(LocalDateTime.of(2021, Month.JANUARY, 24, 23, 59, 59, 0))
     assert(exercise004.getDateTimeWithGigaSecond == LocalDateTime.of(2052, Month.OCTOBER, 3, 1, 46, 39))
   }
+
+  it should "be returned when the resulting date is a leap day" in {
+    val exercise004 = new Exercise004(LocalDateTime.of(1988, Month.JUNE, 22, 12, 0, 0, 0))
+    assert(exercise004.getDateTimeWithGigaSecond == LocalDateTime.of(2020, Month.FEBRUARY, 29, 13, 46, 40))
+  }
+
+  it should "be returned when both original and resulting date are BC" in {
+    val exercise004 = new Exercise004(LocalDateTime.of(-2000, Month.JANUARY, 1, 12, 0, 0, 0))
+    assert(exercise004.getDateTimeWithGigaSecond == LocalDateTime.of(-1969, Month.SEPTEMBER, 9, 13, 46, 40))
+  }
+
+  it should "be returned when original date is BC and resulting date is AC" in {
+    val exercise004 = new Exercise004(LocalDateTime.of(-15, Month.JANUARY, 1, 12, 0, 0, 0))
+    assert(exercise004.getDateTimeWithGigaSecond == LocalDateTime.of(16, Month.SEPTEMBER, 9, 13, 46, 40))
+  }
+
+  it should "be handled correctly when resulting date greater than maximum allowed" in {
+    val exercise004 = new Exercise004(LocalDateTime.of(999999999, Month.DECEMBER, 31, 12, 0, 0, 0))
+    assert(exercise004.getDateTimeWithGigaSecond == LocalDateTime.of(0, Month.JANUARY, 1, 0, 0, 0))
+  }
 }
